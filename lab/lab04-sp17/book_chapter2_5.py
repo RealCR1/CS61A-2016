f81c12f4fa406c4b6f9a5c2dcd5834c0f337de9b
RealCR1 <Wz892477341@outlook.com>
2017-09-26 10:24:02 +0800

    Haved finished the chapter 2.5 of the reference book.Now, starting the lab 04.

--- lab/lab04-sp17/book_chapter2_5.py
+++ lab/lab04-sp17/book_chapter2_5.py
@@ -1,13 +1,49 @@
 class Account:
+    interest = 0.02
     def __init__(self, account_holder):
-    	self.balance = 0
-    	self.holder = account_holder
+        #Oh, it is so important to type two '_' at the both siede of the function.
+        self.balance = 0
+        self.holder = account_holder
     def deposit(self, amount):
-    	self.balance = self.balance + amount
-    	return self.balance
+        self.balance = self.balance + amount
+        return self.balance
     def withdraw(self, amount):
-    	if self.balance < amount:
-    		return 'Insufficient funds.'
-    	else:
-    	    self.balance = self.balance - amount
-    	    return self.balance
+        if self.balance < amount:
+            return 'Insufficient funds.'
+        else:
+            self.balance = self.balance - amount
+            return self.balance
+
+class CheckingAccount(Account):
+    """
+    A bank account that charges for withdrawls.
+    """
+    withdraw_charge = 1
+    interest = 0.01
+    def withdraw(self, amount):
+        return Account.withdraw(self, amount + self.withdraw_charge)
+
+
+class SavingsAccount(Account):
+    """
+    A bank account that charges for deposit.
+    """
+    deposit_charge = 2
+    def deposit(self, amount):
+        return Account.deposit(self, amount - self.deposit_charge)
+
+class AsSeenOnTVAccount(CheckingAccount, SavingsAccount):
+    """docstring for AsSeenOnTVAccount"""
+    def __init__(self, account_holder):
+        self.holder = account_holder
+        self.balance = 1
+
+
+"""
+subclass points to the base class.From AsSeenOnTVAccount-->CheckingAccount -->SavingsAccount
+--> Account -->object
+"""
+
+
+
+        
