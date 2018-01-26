116398a81e8309f04b5bde0b704e502325a79f1b
RealCR1 <Wz892477341@outlook.com>
2018-01-26 23:36:42 +0800

    Part of the discussion 06.

--- discussion--sp17/discuss6/dicussion6.py
+++ discussion--sp17/discuss6/dicussion6.py
@@ -10,3 +10,25 @@ class Naturals():
 	def __iter__(self):
 		return self
 
+
+from operator import add
+evens = IteratorCombiner(Naturals(), Naturals(), add)
+
+class IteratorCombiner(object):
+	def __init__(self, iterator1, iterator2, combiner):
+		self.iter1 = iterator1
+		self.iter2 = iterator2
+		self.combiner = combiner
+	def __next__(self):
+		return self.combiner(next(self.iter1), next(self.iter2))
+
+
+
+	def __iter__(self):
+		return self
+		
+
+
+
+
+
