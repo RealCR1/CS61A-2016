84bf1b3b6a918d1d09f0ba1580da48ad27d65712
RealCR1 <Wz892477341@outlook.com>
2018-02-04 22:40:10 +0800

    Scheme expression.

--- lab/lab09-sp17/lab09/lab09/Chapter3.py
+++ lab/lab09-sp17/lab09/lab09/Chapter3.py
@@ -8,6 +8,10 @@ from buffer import Buffer, InputReader
 
 # Pairs and Scheme lists
 
+####################
+##Expression Trees##
+####################
+
 class Pair(object):
     """A pair has two instance attributes: first and second.  For a Pair to be
     a well-formed list, second is either a well-formed list or nil.  Some
@@ -160,3 +164,148 @@ s = Pair(1, Pair(2, nil))
 print(s)
 print(s.map(lambda x: x+4))
 expr = Pair('+', Pair(Pair('*', Pair(3, Pair(4, nil))), Pair(5, nil)))
+
+
+
+
+########################
+##Parsing Expressions###
+########################
+
+# Part I -- Lexical Analyzer
+"""The scheme_tokens module provides functions tokenize_line and tokenize_lines
+for converting (iterators producing) strings into (iterators producing) lists
+of tokens.  A token may be:
+
+  * A number (represented as an int or float)
+  * A boolean (represented as a bool)
+  * A symbol (represented as a string)
+  * A delimiter, including parentheses, dots, and single quotes
+"""
+
+import string
+import sys
+
+_SYMBOL_STARTS = set('!$%&*/:<=>?@^_~') | set(string.ascii_lowercase)
+_SYMBOL_INNERS = _SYMBOL_STARTS | set(string.digits) | set('+-.')
+_NUMERAL_STARTS = set(string.digits) | set('+-.')
+_WHITESPACE = set(' \t\n\r')
+_SINGLE_CHAR_TOKENS = set("()'")
+_TOKEN_END = _WHITESPACE | _SINGLE_CHAR_TOKENS
+DELIMITERS = _SINGLE_CHAR_TOKENS | {'.'}
+
+def valid_symbol(s):
+    """Returns whether s is not a well-formed value."""
+    if len(s) == 0 or s[0] not in _SYMBOL_STARTS:
+        return False
+    for c in s[1:]:
+        if c not in _SYMBOL_INNERS:
+            return False
+    return True
+
+def next_candidate_token(line, k):
+    """A tuple (tok, k'), where tok is the next substring of line at or
+    after position k that could be a token (assuming it passes a validity
+    check), and k' is the position in line following that token.  Returns
+    (None, len(line)) when there are no more tokens."""
+    while k < len(line):
+        c = line[k]
+        if c == ';':
+            return None, len(line)
+        elif c in _WHITESPACE:
+            k += 1
+        elif c in _SINGLE_CHAR_TOKENS:
+            return c, k+1
+        elif c == '#':  # Boolean values #t and #f
+            return line[k:k+2], min(k+2, len(line))
+        else:
+            j = k
+            while j < len(line) and line[j] not in _TOKEN_END:
+                j += 1
+            return line[k:j], min(j, len(line))
+    return None, len(line)
+
+def tokenize_line(line):
+    """The list of Scheme tokens on line.  Excludes comments and whitespace."""
+    result = []
+    text, i = next_candidate_token(line, 0)
+    while text is not None:
+        if text in DELIMITERS:
+            result.append(text)
+        elif text == '+' or text == '-':
+            result.append(text)
+        elif text == '#t' or text.lower() == 'true':
+            result.append(True)
+        elif text == '#f' or text.lower() == 'false':
+            result.append(False)
+        elif text == 'nil':
+            result.append(text)
+        elif text[0] in _NUMERAL_STARTS:
+            try:
+                result.append(int(text))
+            except ValueError:
+                try:
+                    result.append(float(text))
+                except ValueError:
+                    raise ValueError("invalid numeral: {0}".format(text))
+        elif text[0] in _SYMBOL_STARTS and valid_symbol(text):
+            result.append(text)
+        else:
+            print("warning: invalid token: {0}".format(text), file=sys.stderr)
+            print("    ", line, file=sys.stderr)
+            print(" " * (i+3), "^", file=sys.stderr)
+
+        text, i = next_candidate_token(line, i)
+    return result
+
+def tokenize_lines(input):
+    """An iterator that returns lists of tokens, one for each line of the
+    iterable input sequence."""
+    return map(tokenize_line, input)
+    	return iterative Lines
+
+
+
+def calc_eval(exp):
+	"""
+	Evaluate a Calculator expression.
+	"""
+	if type(exp) in (int, float):
+		return simplify(exp)
+	elif isinstance(exp, Pair):
+		arguments = exp.second.map(calc_eval)
+		return simplify(calc_apply(exp.first, arguments))
+
+	else:
+		raise TypeError(exp + ' is not a number or call expression')
+
+
+def calc_apply(operator, args):
+	"""
+	Apply the named operator to a list of args.
+	"""
+	if not isinstance(operator, str):
+		raise TypeError(str(operator) + ' is not a symbol')
+	if operator == '+':
+		return reduce(add, args, 0)
+	elif operator == '-':
+		if len(args) == 0:
+			raise TypeError(operator + ' requires at least 1 argument')
+		elif len(args) == 1:
+			return -args.first
+            else:
+                return reduce(sub, args.second, args.first)
+        elif operator == '*':
+            return reduce(mul, args, 1)
+        elif operator == '/':
+            if len(args) == 0:
+                raise TypeError(operator + ' requires at least 1 argument')
+            elif len(args) == 1:
+                return 1/args.first
+            else:
+                return reduce(truediv, args.second, args.first)
+        else:
+            raise TypeError(operator + ' is an unknown operator')
+
+
+
