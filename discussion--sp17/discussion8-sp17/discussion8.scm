d6945fead3577ac5cf6ab8a59ff1d40a2a6019c7
RealCR1 <Wz892477341@outlook.com>
2018-02-11 10:12:20 +0800

    Discussion 8, this is for you!

--- discussion--sp17/discussion8-sp17/discussion8.scm
+++ discussion--sp17/discussion8-sp17/discussion8.scm
@@ -15,3 +15,37 @@
 		(+ (fib (- n 1)) (fib (- n 2)))
 	)
 )
+
+
+
+(define (contact a b)
+	(if (null? a) b
+		(cons (car a)
+			(contact (cdr a) b))
+	)
+
+)
+
+;def contact(a, b):
+;	if a = Link.empty:
+;		return b
+;	else:
+;		return Link(a.first, contact(a.rest, b))
+; 
+(contact '(1 2 3) '(4 5 6))
+
+
+
+(define (replicate x n)
+	(if (equal? n 0) nil
+		(cons x (replicate x (- n 1)))
+
+	)
+)
+
+
+; def replicate(x, n):
+; 	if n == 0:
+; 		return Link.empty
+; 	else:
+; 		return Link(x, replicate(x, n-1))
