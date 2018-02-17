8f308845c26590630de018be8defa816025afe23
RealCR1 <Wz892477341@outlook.com>
2018-02-17 23:32:31 +0800

    HHHHHHH! Locking questions about Q1 have been solved.

--- projects/ProjectFour--sp17/scheme/scheme/tests/01.py
+++ projects/ProjectFour--sp17/scheme/scheme/tests/01.py
@@ -52,29 +52,24 @@ test = {
         {
           'code': r"""
           >>> scheme_read(Buffer(tokenize_lines(['(+ 23 4)'])))
-          6c4217f08dee3ab8c574e916e1c826a8
-          # locked
+          Pair('+', Pair(23, Pair(4, nil)))
           >>> read_line('(+ 23 4)')  # Shorter version of above!
-          6c4217f08dee3ab8c574e916e1c826a8
-          # locked
+          Pair('+', Pair(23, Pair(4, nil)))
           """,
           'hidden': False,
-          'locked': True
+          'locked': False
         },
         {
           'code': r"""
           >>> read_tail(Buffer(tokenize_lines([')'])))
-          76b14f86e886c2669945a79c3bc272f4
-          # locked
+          nil
           >>> read_tail(Buffer(tokenize_lines(['2 3)'])))
-          cda024250e0f59f6fb50beedd5aa96a7
-          # locked
+          Pair(2, Pair(3, nil))
           >>> read_tail(Buffer(tokenize_lines(['2 (3 4))'])))
-          b7862fa62d0476ab4f2798cb40bedf50
-          # locked
+          Pair(2, Pair(Pair(3, Pair(4, nil)), nil))
           """,
           'hidden': False,
-          'locked': True
+          'locked': False
         },
         {
           'code': r"""
@@ -89,14 +84,10 @@ test = {
         {
           'code': r"""
           >>> read_line("(+ (- 2 3) 1)")
-          79220544ffe5bd238e524375b10bc4fc
-          # locked
-          # choice: Pair('+', Pair('-', Pair(2, Pair(3, Pair(1, nil)))))
-          # choice: Pair('+', Pair('-', Pair(2, Pair(3, nil))), Pair(1, nil))
-          # choice: Pair('+', Pair(Pair('-', Pair(2, Pair(3, nil))), Pair(1, nil)))
+          Pair('+', Pair(Pair('-', Pair(2, Pair(3, nil))), Pair(1, nil)))
           """,
           'hidden': False,
-          'locked': True
+          'locked': False
         }
       ],
       'scored': True,
