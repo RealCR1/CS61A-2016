6260c6db7440b0dc45607ba462ec1c17ac7281c9
RealCR1 <Wz892477341@outlook.com>
2018-02-18 18:26:10 +0800

    Question 3 in Part II get it!

--- projects/ProjectFour--sp17/scheme/scheme/scheme.py
+++ projects/ProjectFour--sp17/scheme/scheme/scheme.py
@@ -74,14 +74,21 @@ class Frame:
         """Define Scheme SYMBOL to have VALUE."""
         # BEGIN PROBLEM 3
         "*** REPLACE THIS LINE ***"
+        self.bindings[symbol] = value
         # END PROBLEM 3
+
 
     def lookup(self, symbol):
         """Return the value bound to SYMBOL. Errors if SYMBOL is not found."""
         # BEGIN PROBLEM 3
         "*** REPLACE THIS LINE ***"
+        if symbol in self.bindings:
+            return self.bindings[symbol]
+        elif self.parent:
+            return self.parent.lookup(symbol)
         # END PROBLEM 3
         raise SchemeError('unknown identifier: {0}'.format(symbol))
+
 
     def make_child_frame(self, formals, vals):
         """Return a new local frame whose parent is SELF, in which the symbols
